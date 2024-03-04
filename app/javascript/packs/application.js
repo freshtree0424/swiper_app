3c7660774a770e174c386837df3daf82d7c70d9a
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-03-04 07:50:45 +0000

    [Add]スライドショー追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import 'jquery'
 import "script.js"
+import Swiper from 'swiper/swiper-bundle.js'; 
+import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
